fae1cb8d8ce7dea0eca00e8369fe6f7f6ec3ae64
ab-ruh <abell.demere@gmail.com>
2024-02-27 18:37:11 +0300

    n

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * prints a message to the console.
+ * @param {string} message - The message to print.
+ * @author Iyasu Asnake <https://github.com/ab-ruh>
+ */
 const displayMessage = (str) => console.log(str);
 
 module.exports = displayMessage;
